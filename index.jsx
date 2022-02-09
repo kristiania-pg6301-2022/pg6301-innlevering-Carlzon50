7741254e7047057b88040f77a2abc2f9fa7fb76b
Carlzon50 <72607945+Carlzon50@users.noreply.github.com>
2022-02-09 15:38:12 +0100

    questions.js

--- index.jsx
+++ index.jsx
@@ -1,7 +1,46 @@
 import * as ReactDOM from "react-dom";
 import * as React from "react";
+import {useState} from "react";
+
+import {isCorrectAnswer, randomQuestion} from "./questions";
+
+function ShowQuestion({question, onAnswer}) {
+    return <>
+        <h1>{question.question}</h1>
+        {Object.keys(question.answers)
+            .filter(a => question.answers[a])
+            .map(a => <p key={a}>
+                <button onClick={() => onAnswer(a)}>{question.answers[a]}</button>
+            </p>)}
+    </>;
+}
+
+function ShowAnswerStatus({answer, onRestart, question}) {
+    return <>
+        <h1>{isCorrectAnswer(question, answer) ? "Right" : "Wrong"}</h1>
+        <p>
+            <button onClick={onRestart}>Another question</button>
+        </p>
+    </>;
+}
+
+function Quiz() {
+    const [question, setQuestion] = useState(randomQuestion());
+    const [answer, setAnswer] = useState();
+
+    function handleRestart() {
+        setQuestion(randomQuestion());
+        setAnswer(undefined);
+    }
+
+    if (answer) {
+        return <ShowAnswerStatus question={question} answer={answer} onRestart={handleRestart}/>;
+    }
+
+    return <ShowQuestion question={question} onAnswer={setAnswer}/>;
+}
 
 ReactDOM.render(
-    <h1>Hello React</h1>,
+    <Quiz/>,
     document.getElementById("app")
 )
