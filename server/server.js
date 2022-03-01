bfedc598e1204ba9002d951c6a6a9485ac2cc18d
Carlzon50 <72607945+Carlzon50@users.noreply.github.com>
2022-03-01 14:58:30 +0100

    json

--- server/server.js
+++ server/server.js
@@ -1,5 +1,23 @@
-import express from express;
+import express from "express";
+import { randomQuestion } from "./questions.js";
+import * as path from "path";
 
 const app = express();
 
-app.listen(1234);
+app.get("/question/random", (req, res) => {
+    const question = randomQuestion();
+    res.json(question);
+});
+
+app.use(express.static(path.resolve("../client/dist")));
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        return res.sendFile(path.resolve("../client/dist/index.html"));
+    } else {
+        next();
+    }
+});
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log(`server started on http://localhost:${server.address().port}`);
+});
