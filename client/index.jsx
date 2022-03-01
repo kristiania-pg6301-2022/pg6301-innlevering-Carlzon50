d1d3354206269eb44f3ba92d057cda8ae0efa7d9
Carlzon50 <72607945+Carlzon50@users.noreply.github.com>
2022-03-01 12:17:12 +0100

    json files

--- client/index.jsx
+++ client/index.jsx
@@ -2,7 +2,7 @@ import * as ReactDOM from "react-dom";
 import * as React from "react";
 import {useState} from "react";
 
-import {isCorrectAnswer, randomQuestion} from "./questions";
+import {isCorrectAnswer, randomQuestion} from "../server/questions";
 
 function ShowQuestion({question, onAnswer}) {
     return <>
