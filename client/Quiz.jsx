a9645ea2d5403348cd8ebfa815612e25e53ef471
Carlzon50 <72607945+Carlzon50@users.noreply.github.com>
2022-03-04 15:43:55 +0100

    make it better

--- client/Quiz.jsx
+++ client/Quiz.jsx
@@ -2,84 +2,83 @@ import * as React from "react";
 import { useState, useEffect } from "react";
 import { Link, Routes, Route, BrowserRouter } from "react-router-dom";
 
-
 function FrontPage() {
-    return (
-        <div>
-            <h1>Quiz App</h1>
-            <ul>
-                <li>
-                    <Link to={"/question"}>Start Your Quiz</Link>
-                </li>
-            </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Quiz App</h1>
+      <ul>
+        <li>
+          <Link to={"/question"}>Start Your Quiz</Link>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 async function fetchJSON() {
-    const res = await fetch("api/question");
-    if (res.ok) {
-        return await res.json();
-    }
+  const res = await fetch("api/question");
+  if (res.ok) {
+    return await res.json();
+  }
 }
 
 function Quiz() {
-    const [question, setQuestion] = useState();
-    const [answer, setAnswer] = useState();
+  const [question, setQuestion] = useState();
+  const [answer, setAnswer] = useState();
 
-    useEffect(() => {
-        async function fetchData() {
-            const res = await fetchJSON("/api/question");
-            setQuestion(res);
-        }
-        fetchData();
-    }, []);
-
-    async function handleAnswer(answer) {
-        const { id } = question;
-        const res = await fetch("/api/question", {
-            method: "post",
-            body: JSON.stringify({ answer, id }),
-            headers: {
-                "content-type": "application/json",
-            },
-        });
-        setAnswer(await res.json());
-        setQuestion(await fetchJSON("/api/question"));
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetchJSON("/api/question");
+      setQuestion(res);
     }
+    fetchData();
+  }, []);
 
-    if (!question) {
-        return <p>Loading...</p>;
-    }
-    return (
-        <>
-            <h1>{question.question}</h1>
-            {Object.keys(question.answers)
-                .filter((a) => question.answers[a])
-                .map((a) => (
-                    <p key={a}>
-                        <button onClick={() => handleAnswer(a)}>
-                            {question.answers[a]}
-                        </button>
-                    </p>
-                ))}
+  async function handleAnswer(answer) {
+    const { id } = question;
+    const res = await fetch("/api/question", {
+      method: "post",
+      body: JSON.stringify({ answer, id }),
+      headers: {
+        "content-type": "application/json",
+      },
+    });
+    setAnswer(await res.json());
+    setQuestion(await fetchJSON("/api/question"));
+  }
 
-            <h2>
-                {answer
-                    ? "Your answer on the last question:" + answer.result.toString()
-                    : null}
-            </h2>
-        </>
-    );
+  if (!question) {
+    return <p>Loading...</p>;
+  }
+  return (
+    <>
+      <h1>{question.question}</h1>
+      {Object.keys(question.answers)
+        .filter((a) => question.answers[a])
+        .map((a) => (
+          <p key={a}>
+            <button onClick={() => handleAnswer(a)}>
+              {question.answers[a]}
+            </button>
+          </p>
+        ))}
+
+      <h2>
+        {answer
+          ? "Your answer on the last question:" + answer.result.toString()
+          : null}
+      </h2>
+    </>
+  );
 }
 
 export function App() {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path={"/"} element={<FrontPage />}></Route>
-                <Route path={"/question"} element={<Quiz />}></Route>
-            </Routes>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />}></Route>
+        <Route path={"/question"} element={<Quiz />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
